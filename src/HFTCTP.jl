726ade18ba77d0ac91fd14b7403c32a92886ba64
linan <linanisyugioh@163.com>
2025-12-29 21:50:29 +0800

    尝试全局变量

--- src/HFTCTP.jl
+++ src/HFTCTP.jl
@@ -427,7 +427,7 @@ global on_strategy_trading_span::Union{Nothing, Function} = nothing
 global on_strategy_trading_span_c::Ptr{Cvoid} = C_NULL
 function strategy_set_trading_span_callback(on_strategy_trading_span_func::Function, user_data::Ptr{Cvoid}=C_NULL)
     global on_strategy_trading_span = on_strategy_trading_span_func
-    global on_strategy_trading_span_c = @cfunction(on_strategy_trading_span, Cvoid, (UInt8, Cint, Cint, Cint, Cint, Ptr{UInt8}, Ptr{Cvoid}))
+    global on_strategy_trading_span_c = @cfunction($on_strategy_trading_span_func, Cvoid, (UInt8, Cint, Cint, Cint, Cint, Ptr{UInt8}, Ptr{Cvoid}))
     sym = Libc.Libdl.dlsym(lib, :strategy_set_trading_span_callback)
     ccall(sym, Cvoid, (Ptr{Cvoid}, Ptr{Cvoid}), on_strategy_trading_span_c, user_data)
     return nothing
@@ -459,8 +459,8 @@ global on_strategy_trading_day::Union{Nothing, Function} = nothing
 """
 global on_strategy_trading_day_c::Ptr{Cvoid} = C_NULL
 function strategy_set_trading_day_callback(on_strategy_trading_day_func::Function, user_data::Ptr{Cvoid}=C_NULL)
-    global on_strategy_trading_day = on_strategy_trading_day_func
-    global on_strategy_trading_day_c = @cfunction(on_strategy_trading_day, Cvoid, (Cint, Cint, Cint, UInt8, Ptr{Cvoid}))
+#    global on_strategy_trading_day = on_strategy_trading_day_func
+    global on_strategy_trading_day_c = @cfunction($on_strategy_trading_day_func, Cvoid, (Cint, Cint, Cint, UInt8, Ptr{Cvoid}))
     sym = Libc.Libdl.dlsym(lib, :strategy_set_trading_day_callback)
     ccall(sym, Cvoid, (Ptr{Cvoid}, Ptr{Cvoid}), on_strategy_trading_day_c, user_data)
     return nothing
